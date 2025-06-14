4ba5b36be6a40966a7e46d375b34678322d0f027
Samkmv <46203434+Samkmv@users.noreply.github.com>
2025-06-14 22:37:58 +0300

    Update default.php

--- app/Views/layouts/default.php
+++ app/Views/layouts/default.php
@@ -263,84 +263,92 @@
 <?= $this->content; ?>
 
 <!-- Page footer -->
-<footer class="footer bg-dark pb-4" data-bs-theme="dark">
+<footer class="footer position-relative bg-dark">
+    <span class="position-absolute top-0 start-0 w-100 h-100 bg-body d-none d-block-dark"></span>
+    <div class="container position-relative z-1 pt-sm-2 pt-md-3 pt-lg-4" data-bs-theme="dark">
 
-    <div class="container py-4 py-md-5">
-        <div class="row pt-3 pb-4 py-md-1 py-lg-3">
-
-            <!-- Promo text + Social account links -->
-            <div class="col-lg-3 text-center text-lg-start pb-sm-2 pb-md-0 mb-4 mb-md-5 mb-lg-0">
-                <h4 class="pb-2 mb-1">
-                    <a class="text-dark-emphasis text-decoration-none" href="index.html">Cartzilla</a>
-                </h4>
-                <p class="fs-sm text-body mx-auto" style="max-width: 480px">With a wide selection of fresh produce, pantry staples, and household essentials, we've got everything you need just a click away.</p>
-                <div class="d-flex justify-content-center justify-content-lg-start gap-2 pt-2 pt-md-3">
-                    <a class="btn btn-icon fs-base btn-outline-secondary border-0" href="#!" data-bs-toggle="tooltip" data-bs-template="<div class=&quot;tooltip fs-xs mb-n2&quot; role=&quot;tooltip&quot;><div class=&quot;tooltip-inner bg-transparent text-white p-0&quot;></div></div>" title="Instagram" aria-label="Follow us on Instagram">
-                        <i class="ci-instagram"></i>
-                    </a>
-                    <a class="btn btn-icon fs-base btn-outline-secondary border-0" href="#!" data-bs-toggle="tooltip" data-bs-template="<div class=&quot;tooltip fs-xs mb-n2&quot; role=&quot;tooltip&quot;><div class=&quot;tooltip-inner bg-transparent text-white p-0&quot;></div></div>" title="Facebook" aria-label="Follow us on Facebook">
-                        <i class="ci-facebook"></i>
-                    </a>
-                    <a class="btn btn-icon fs-base btn-outline-secondary border-0" href="#!" data-bs-toggle="tooltip" data-bs-template="<div class=&quot;tooltip fs-xs mb-n2&quot; role=&quot;tooltip&quot;><div class=&quot;tooltip-inner bg-transparent text-white p-0&quot;></div></div>" title="Telegram" aria-label="Follow us on Telegram">
-                        <i class="ci-telegram"></i>
-                    </a>
-                    <a class="btn btn-icon fs-base btn-outline-secondary border-0" href="#!" data-bs-toggle="tooltip" data-bs-template="<div class=&quot;tooltip fs-xs mb-n2&quot; role=&quot;tooltip&quot;><div class=&quot;tooltip-inner bg-transparent text-white p-0&quot;></div></div>" title="WhatsApp" aria-label="Follow us on WhatsApp">
-                        <i class="ci-whatsapp"></i>
-                    </a>
+        <!-- Columns with links that are turned into accordion on screens < 500px wide (sm breakpoint) -->
+        <div class="accordion py-5" id="footerLinks">
+            <div class="row">
+                <div class="col-md-4 d-sm-flex flex-md-column align-items-center align-items-md-start pb-3 mb-sm-4">
+                    <h4 class="mb-sm-0 mb-md-4 me-4">
+                        <a class="text-dark-emphasis text-decoration-none" href="index.html">Cartzilla</a>
+                    </h4>
+                    <p class="text-body fs-sm text-sm-end text-md-start mb-sm-0 mb-md-3 ms-0 ms-sm-auto ms-md-0 me-4">Got questions? Contact us 24/7</p>
+                    <div class="dropdown" style="max-width: 250px">
+                        <button type="button" class="btn btn-secondary dropdown-toggle justify-content-between w-100" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Help and consultation
+                        </button>
+                        <ul class="dropdown-menu" style="">
+                            <li><a class="dropdown-item" href="#!">Help center &amp; FAQ</a></li>
+                            <li><a class="dropdown-item" href="#!">Support chat</a></li>
+                            <li><a class="dropdown-item" href="#!">Open support ticket</a></li>
+                            <li><a class="dropdown-item" href="#!">Call center</a></li>
+                        </ul>
+                    </div><div class="d-flex justify-content-center justify-content-lg-start gap-2 pt-2 pt-md-3">
+                        <a class="btn btn-icon fs-base btn-outline-secondary border-0" href="#!" data-bs-toggle="tooltip" data-bs-template="<div class=&quot;tooltip fs-xs mb-n2&quot; role=&quot;tooltip&quot;><div class=&quot;tooltip-inner bg-transparent text-white p-0&quot;></div></div>" aria-label="Follow us on Instagram" data-bs-original-title="Instagram">
+                            <i class="ci-instagram"></i>
+                        </a>
+                        <a class="btn btn-icon fs-base btn-outline-secondary border-0" href="#!" data-bs-toggle="tooltip" data-bs-template="<div class=&quot;tooltip fs-xs mb-n2&quot; role=&quot;tooltip&quot;><div class=&quot;tooltip-inner bg-transparent text-white p-0&quot;></div></div>" aria-label="Follow us on Facebook" data-bs-original-title="Facebook">
+                            <i class="ci-facebook"></i>
+                        </a>
+                        <a class="btn btn-icon fs-base btn-outline-secondary border-0" href="#!" data-bs-toggle="tooltip" data-bs-template="<div class=&quot;tooltip fs-xs mb-n2&quot; role=&quot;tooltip&quot;><div class=&quot;tooltip-inner bg-transparent text-white p-0&quot;></div></div>" aria-label="Follow us on Telegram" data-bs-original-title="Telegram">
+                            <i class="ci-telegram"></i>
+                        </a>
+                        <a class="btn btn-icon fs-base btn-outline-secondary border-0" href="#!" data-bs-toggle="tooltip" data-bs-template="<div class=&quot;tooltip fs-xs mb-n2&quot; role=&quot;tooltip&quot;><div class=&quot;tooltip-inner bg-transparent text-white p-0&quot;></div></div>" aria-label="Follow us on WhatsApp" data-bs-original-title="WhatsApp">
+                            <i class="ci-whatsapp"></i>
+                        </a>
+                    </div>
                 </div>
-            </div>
-
-            <!-- Columns with links that are turned into accordion on screens < 500px wide (sm breakpoint) -->
-            <div class="col-lg-8 offset-lg-1">
-                <div class="accordion" id="footerLinks">
-                    <div class="row">
-                        <div class="accordion-item col-sm-3 border-0">
+                <div class="col-md-8">
+                    <div class="row row-cols-1 row-cols-sm-3 gx-3 gx-md-4">
+                        <div class="accordion-item col border-0">
                             <h6 class="accordion-header" id="companyHeading">
                                 <span class="text-dark-emphasis d-none d-sm-block">Company</span>
-                                <button type="button" class="accordion-button collapsed py-3 d-sm-none" data-bs-toggle="collapse" data-bs-target="#companyLinks" aria-expanded="false" aria-controls="companyLinks">Company</button>
+                                <button type="button" class="accordion-button py-3 d-sm-none collapsed" data-bs-toggle="collapse" data-bs-target="#companyLinks" aria-expanded="false" aria-controls="companyLinks">Company</button>
                             </h6>
-                            <div class="accordion-collapse collapse d-sm-block" id="companyLinks" aria-labelledby="companyHeading" data-bs-parent="#footerLinks">
-                                <ul class="nav flex-column gap-2 pt-sm-3 pb-3 pb-sm-0 mt-n1 mb-1 mb-sm-0">
+                            <div class="accordion-collapse d-sm-block collapse" id="companyLinks" aria-labelledby="companyHeading" data-bs-parent="#footerLinks" style="">
+                                <ul class="nav flex-column gap-2 pt-sm-3 pb-3 mt-n1 mb-1">
                                     <li class="d-flex w-100 pt-1">
-                                        <a class="nav-link animate-underline animate-target d-inline fw-normal text-truncate p-0" href="#!">Blog and news</a>
+                                        <a class="nav-link animate-underline animate-target d-inline fw-normal text-truncate p-0" href="#!">About company</a>
                                     </li>
                                     <li class="d-flex w-100 pt-1">
-                                        <a class="nav-link animate-underline animate-target d-inline fw-normal text-truncate p-0" href="#!">About us</a>
+                                        <a class="nav-link animate-underline animate-target d-inline fw-normal text-truncate p-0" href="#!">Our team</a>
                                     </li>
                                     <li class="d-flex w-100 pt-1">
-                                        <a class="nav-link animate-underline animate-target d-inline fw-normal text-truncate p-0" href="#!">FAQ page</a>
+                                        <a class="nav-link animate-underline animate-target d-inline fw-normal text-truncate p-0" href="#!">Careers</a>
                                     </li>
                                     <li class="d-flex w-100 pt-1">
                                         <a class="nav-link animate-underline animate-target d-inline fw-normal text-truncate p-0" href="#!">Contact us</a>
                                     </li>
                                     <li class="d-flex w-100 pt-1">
-                                        <a class="nav-link animate-underline animate-target d-inline fw-normal text-truncate p-0" href="#!">Careers</a>
+                                        <a class="nav-link animate-underline animate-target d-inline fw-normal text-truncate p-0" href="#!">News</a>
                                     </li>
                                 </ul>
                             </div>
                             <hr class="d-sm-none my-0">
                         </div>
-                        <div class="accordion-item col-sm-3 border-0">
+                        <div class="accordion-item col border-0">
                             <h6 class="accordion-header" id="accountHeading">
                                 <span class="text-dark-emphasis d-none d-sm-block">Account</span>
-                                <button type="button" class="accordion-button collapsed py-3 d-sm-none" data-bs-toggle="collapse" data-bs-target="#accountLinks" aria-expanded="false" aria-controls="accountLinks">Account</button>
+                                <button type="button" class="accordion-button py-3 d-sm-none collapsed" data-bs-toggle="collapse" data-bs-target="#accountLinks" aria-expanded="false" aria-controls="accountLinks">Account</button>
                             </h6>
-                            <div class="accordion-collapse collapse d-sm-block" id="accountLinks" aria-labelledby="accountHeading" data-bs-parent="#footerLinks">
-                                <ul class="nav flex-column gap-2 pt-sm-3 pb-3 pb-sm-0 mt-n1 mb-1 mb-sm-0">
+                            <div class="accordion-collapse d-sm-block collapse" id="accountLinks" aria-labelledby="accountHeading" data-bs-parent="#footerLinks" style="">
+                                <ul class="nav flex-column gap-2 pt-sm-3 pb-3 mt-n1 mb-1">
                                     <li class="d-flex w-100 pt-1">
                                         <a class="nav-link animate-underline animate-target d-inline fw-normal text-truncate p-0" href="#!">Your account</a>
                                     </li>
                                     <li class="d-flex w-100 pt-1">
-                                        <a class="nav-link animate-underline animate-target d-inline fw-normal text-truncate p-0" href="#!">Shipping &amp; policies</a>
+                                        <a class="nav-link animate-underline animate-target d-inline fw-normal text-truncate p-0" href="#!">Shipping rates &amp; policies</a>
                                     </li>
                                     <li class="d-flex w-100 pt-1">
                                         <a class="nav-link animate-underline animate-target d-inline fw-normal text-truncate p-0" href="#!">Refunds &amp; replacements</a>
                                     </li>
                                     <li class="d-flex w-100 pt-1">
-                                        <a class="nav-link animate-underline animate-target d-inline fw-normal text-truncate p-0" href="#!">Order tracking</a>
+                                        <a class="nav-link animate-underline animate-target d-inline fw-normal text-truncate p-0" href="#!">Delivery info</a>
                                     </li>
                                     <li class="d-flex w-100 pt-1">
-                                        <a class="nav-link animate-underline animate-target d-inline fw-normal text-truncate p-0" href="#!">Delivery info</a>
+                                        <a class="nav-link animate-underline animate-target d-inline fw-normal text-truncate p-0" href="#!">Order tracking</a>
                                     </li>
                                     <li class="d-flex w-100 pt-1">
                                         <a class="nav-link animate-underline animate-target d-inline fw-normal text-truncate p-0" href="#!">Taxes &amp; fees</a>
@@ -349,13 +357,13 @@
                             </div>
                             <hr class="d-sm-none my-0">
                         </div>
-                        <div class="accordion-item col-sm-3 border-0">
+                        <div class="accordion-item col border-0">
                             <h6 class="accordion-header" id="customerHeading">
                                 <span class="text-dark-emphasis d-none d-sm-block">Customer service</span>
-                                <button type="button" class="accordion-button collapsed py-3 d-sm-none" data-bs-toggle="collapse" data-bs-target="#customerLinks" aria-expanded="false" aria-controls="customerLinks">Customer service</button>
+                                <button type="button" class="accordion-button py-3 d-sm-none collapsed" data-bs-toggle="collapse" data-bs-target="#customerLinks" aria-expanded="false" aria-controls="customerLinks">Customer service</button>
                             </h6>
-                            <div class="accordion-collapse collapse d-sm-block" id="customerLinks" aria-labelledby="customerHeading" data-bs-parent="#footerLinks">
-                                <ul class="nav flex-column gap-2 pt-sm-3 pb-3 pb-sm-0 mt-n1 mb-1 mb-sm-0">
+                            <div class="accordion-collapse d-sm-block collapse" id="customerLinks" aria-labelledby="customerHeading" data-bs-parent="#footerLinks" style="">
+                                <ul class="nav flex-column gap-2 pt-sm-3 pb-3 mt-n1 mb-1">
                                     <li class="d-flex w-100 pt-1">
                                         <a class="nav-link animate-underline animate-target d-inline fw-normal text-truncate p-0" href="#!">Payment methods</a>
                                     </li>
@@ -363,16 +371,16 @@
                                         <a class="nav-link animate-underline animate-target d-inline fw-normal text-truncate p-0" href="#!">Money back guarantee</a>
                                     </li>
                                     <li class="d-flex w-100 pt-1">
-                                        <a class="nav-link animate-underline animate-target d-inline fw-normal text-truncate p-0" href="#!">Refunds &amp; replacements</a>
+                                        <a class="nav-link animate-underline animate-target d-inline fw-normal text-truncate p-0" href="#!">Product returns</a>
                                     </li>
                                     <li class="d-flex w-100 pt-1">
-                                        <a class="nav-link animate-underline animate-target d-inline fw-normal text-truncate p-0" href="#!">Order tracking</a>
-                                    </li>
-                                    <li class="d-flex w-100 pt-1">
-                                        <a class="nav-link animate-underline animate-target d-inline fw-normal text-truncate p-0" href="#!">Delivery info</a>
+                                        <a class="nav-link animate-underline animate-target d-inline fw-normal text-truncate p-0" href="#!">Support center</a>
                                     </li>
                                     <li class="d-flex w-100 pt-1">
                                         <a class="nav-link animate-underline animate-target d-inline fw-normal text-truncate p-0" href="#!">Shipping</a>
+                                    </li>
+                                    <li class="d-flex w-100 pt-1">
+                                        <a class="nav-link animate-underline animate-target d-inline fw-normal text-truncate p-0" href="#!">Terms &amp; conditions</a>
                                     </li>
                                 </ul>
                             </div>
@@ -382,12 +390,32 @@
                 </div>
             </div>
         </div>
-    </div>
 
-    <!-- Copyright -->
-    <p class="container fs-xs text-body text-center text-lg-start pb-md-3 mb-0">
-        © Все права защищены <?= date("Y")?> г.
-    </p>
+        <!-- Category / tag links -->
+
+
+        <!-- Copyright + Payment methods -->
+        <div class="d-md-flex align-items-center border-top py-4">
+            <div class="d-flex gap-2 gap-sm-3 justify-content-center ms-md-auto mb-4 mb-md-0 order-md-2">
+                <div>
+                    <img src="<?= base_url('/assets/default/img/payment-methods/visa-dark-mode.svg') ?>" alt="Visa">
+                </div>
+                <div>
+                    <img src="<?= base_url('/assets/default/img/payment-methods/mastercard.svg') ?>" alt="Mastercard">
+                </div>
+                <div>
+                    <img src="<?= base_url('/assets/default/img/payment-methods/paypal-dark-mode.svg') ?>" alt="PayPal">
+                </div>
+                <div>
+                    <img src="<?= base_url('/assets/default/img/payment-methods/google-pay-dark-mode.svg') ?>" alt="Google Pay">
+                </div>
+                <div>
+                    <img src="<?= base_url('/assets/default/img/payment-methods/apple-pay-dark-mode.svg') ?>" alt="Apple Pay">
+                </div>
+            </div>
+            <p class="text-body fs-xs text-center text-md-start mb-0 me-4 order-md-1">© All rights reserved. <?= date('Y'); ?> г.</p>
+        </div>
+    </div>
 </footer>
 
 <!-- Back to top button -->
