5c1c4f256fd3568b7f565555810a9732b9c37090
Samkmv <46203434+Samkmv@users.noreply.github.com>
2025-06-23 20:13:59 +0300

    Add Forbidden page

--- core/Middleware/Auth.php
+++ core/Middleware/Auth.php
@@ -9,7 +9,9 @@ class Auth
     {
         if (!check_auth()) {
 
-            session()->setFlash('error', 'You must be logged in to access this page');
+            session()->setFlash('error', 'You must be logged in to access this page.');
+
+            abort('You must be logged in to access this page.', 403);
 
             response()->redirect('/login');
         }
