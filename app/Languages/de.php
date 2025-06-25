54e4ed67a0600212b5a93bfa922acedfbe7f5b8c
Samkmv <46203434+Samkmv@users.noreply.github.com>
2025-06-25 23:04:14 +0300

    Lang update

--- app/Languages/de.php
+++ app/Languages/de.php
@@ -4,9 +4,4 @@ return [
     'tpl_logo' => 'Логотип',
     'tpl_security_error' => 'Ошибка токена',
     'menu_home' => 'Startseite',
-    'menu_dashboard' => 'Dashboard',
-    'menu_register' => 'Registrieren',
-    'menu_login' => 'Login',
-    'menu_users' => 'Internetnutzer',
-    'menu_posts' => 'Beiträge',
 ];
--- app/Languages/ru.php
+++ app/Languages/ru.php
@@ -4,11 +4,4 @@ return [
     'tpl_logo' => 'Логотип',
     'tpl_security_error' => 'Ошибка токена',
     'menu_home' => 'Главная',
-    'menu_dashboard' => 'Панель управления',
-    'menu_register' => 'Регистрация',
-    'menu_login' => 'Вход',
-    'menu_logout' => 'Выход',
-    'menu_users' => 'Пользователи',
-    'menu_posts' => 'Новости',
-    'menu_contact' => 'Контакты',
 ];
