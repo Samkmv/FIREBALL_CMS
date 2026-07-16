85b732bedc9d9eefa35291fc75a209e1e33a98de
Samkmv <46203434+Samkmv@users.noreply.github.com>
2026-07-16 13:38:51 +0300

    Update

--- config/version.php
+++ config/version.php
@@ -2,7 +2,7 @@
 
 return [
     'name' => 'FIREBALL_CMS',
-    'version' => '1.7.2-beta.14',
+    'version' => '1.7.2-beta.15',
 //    'version' => '1.7.4',
     'released_at' => '2026-07-15',
     'summary' => 'Стабильный релиз FIREBALL CMS',
